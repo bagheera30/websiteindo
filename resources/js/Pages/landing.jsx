62c1b24e31781319d12f74ef675f592ac22ff425
bagheera30 <rizkialfian30103@gmail.com>
2022-07-13 16:43:57 +0700

    landing

--- resources/js/Pages/landing.jsx
+++ resources/js/Pages/landing.jsx
@@ -6,8 +6,12 @@ export default function landing(props){
         <>
         <head>your rebahan</head>
         <div className="">
-            <div>
-                YOUR
+            <div className="mt-4 flex">
+                YOUR<svg width="20" height="20
+                " viewBox="0 0 41 47" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M23.1525 4.17799C29.0947 6.16767 33.3768 11.7794 33.3768 18.3913V20.4348V25.2029V31.692L37.4638 36.7344V40.1884L40.2633 40.1884L33.3768 40.1884L18.3913 40.1884L3.4058 40.1884H0V36.7344L3.4058 32.5324V25.2029V20.4348V18.3913C3.4058 11.5231 8.02627 5.73414 14.3284 3.9631C14.5606 1.73613 16.4435 0 18.7319 0C21.0934 0 23.0231 1.84882 23.1525 4.17799ZM14.3043 41.5507V42.5725C14.3043 45.0177 16.2866 47 18.7319 47C21.1771 47 23.1594 45.0177 23.1594 42.5725V41.5507H14.3043Z" fill="#67686C"/>
+                </svg>
+
             </div>
             <div>
             REBAHAN
