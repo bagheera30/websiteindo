0ed032491f7bf73a5a87f890499684c4480a8b33
bagheera30 <rizkialfian30103@gmail.com>
2022-07-13 15:59:41 +0700

    landing

---
+++ resources/js/Pages/landing.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link, Head } from '@inertiajs/inertia-react';
+
+export default function landing(props){
+    return(
+        <>
+        <head>your rebahan</head>
+        <div className="">
+            your
+        </div>
+        </>
+    )
+};
