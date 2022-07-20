1028056952fb4bd8dfa5c19da60e89cdc4e054c6
bagheera30 <rizkialfian30103@gmail.com>
2022-07-20 21:49:11 +0700

    indo comiit

--- resources/js/Pages/landing.jsx
+++ resources/js/Pages/landing.jsx
@@ -5,21 +5,30 @@ export default function landing(props) {
     return (
         <>
             <Head title={props.judul} />
-            <div className="my-20  mx-10 text-2xl font-serif ">
-                <h1 className="my-10  mx-10 text-6xl font-bold ">
-                    <p className="flex">{props.a}<svg width="80" height="80" viewBox="0 0 41 47" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" clip-rule="evenodd" d="M23.1525 4.17799C29.0947 6.16767 33.3768 11.7794 33.3768 18.3913V20.4348V25.2029V31.692L37.4638 36.7344V40.1884L40.2633 40.1884L33.3768 40.1884L18.3913 40.1884L3.4058 40.1884H0V36.7344L3.4058 32.5324V25.2029V20.4348V18.3913C3.4058 11.5231 8.02627 5.73414 14.3284 3.9631C14.5606 1.73613 16.4435 0 18.7319 0C21.0934 0 23.0231 1.84882 23.1525 4.17799ZM14.3043 41.5507V42.5725C14.3043 45.0177 16.2866 47 18.7319 47C21.1771 47 23.1594 45.0177 23.1594 42.5725V41.5507H14.3043Z" fill="#67686C" />
-                    </svg>
-                    </p>
-                    <p className="mt-2">{props.b}</p>
-                    <p className="mt-2">{props.c}</p>
-                </h1>
-                <h2>
-                    <p>Your rebahan partner is a new way  manage and notify you about</p>
-                    <p> your tasks. Not just it, we also helps you to lighten your mood, </p>
-                    <p> motivate, and support you to work on better time management.</p>
-                </h2>
+            <div classname="carousel rounded-box">
+                <div classname="carousel-item">
+                    <img src="https://placeimg.com/400/300/arch" alt="Burger" />
+                </div>
+                <div classname="carousel-item">
+                    <img src="https://placeimg.com/400/300/arch" alt="Burger" />
+                </div>
+                <div classname="carousel-item">
+                    <img src="https://placeimg.com/400/300/arch" alt="Burger" />
+                </div>
+                <div classname="carousel-item">
+                    <img src="https://placeimg.com/400/300/arch" alt="Burger" />
+                </div>
+                <div classname="carousel-item">
+                    <img src="https://placeimg.com/400/300/arch" alt="Burger" />
+                </div>
+                <div classname="carousel-item">
+                    <img src="https://placeimg.com/400/300/arch" alt="Burger" />
+                </div>
+                <div classname="carousel-item">
+                    <img src="https://placeimg.com/400/300/arch" alt="Burger" />
+                </div>
             </div>
+            # Snap to center
 
         </>
     )
