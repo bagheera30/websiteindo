ef7d119efe520243d9b05af1501f71d0eabb923b
bagheera30 <rizkialfian30103@gmail.com>
2022-07-21 22:34:55 +0700

    update

---
+++ app/Http/Controllers/BiouserController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\biouser;
+use Illuminate\Http\Request;
+
+class BiouserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\biouser  $biouser
+     * @return \Illuminate\Http\Response
+     */
+    public function show(biouser $biouser)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\biouser  $biouser
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(biouser $biouser)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\biouser  $biouser
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, biouser $biouser)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\biouser  $biouser
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(biouser $biouser)
+    {
+        //
+    }
+}
