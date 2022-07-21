c1401a4e781183ee1f6413a2a7b6107da863a859
bagheera30 <rizkialfian30103@gmail.com>
2022-07-21 22:51:27 +0700

    update

--- resources/js/Components/Navbar.jsx
+++ resources/js/Components/Navbar.jsx
@@ -1,31 +1,34 @@
 const Navbar = () => {
     return (
-        <div class="navbar bg-base-100">
-            <div class="flex-1">
-                <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
+        <div className="navbar bg-base-100">
+            <div className="flex-1">
+                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
             </div>
-            <div class="flex-none gap-2">
-                <div class="form-control">
+            <div className="flex-none gap-2">
+                <div className="form-control">
                     <input
                         type="text"
                         placeholder="Search"
-                        class="input input-bordered"
+                        className="input input-bordered"
                     />
                 </div>
-                <div class="dropdown dropdown-end">
-                    <label tabindex="0" class="btn btn-ghost btn-circle avatar">
-                        <div class="w-10 rounded-full">
+                <div className="dropdown dropdown-end">
+                    <label
+                        tabindex="0"
+                        className="btn btn-ghost btn-circle avatar"
+                    >
+                        <div className="w-10 rounded-full">
                             <img src="https://placeimg.com/80/80/people" />
                         </div>
                     </label>
                     <ul
                         tabindex="0"
-                        class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+                        className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
                     >
                         <li>
-                            <a class="justify-between">
+                            <a className="justify-between">
                                 Profile
-                                <span class="badge">New</span>
+                                <span className="badge">New</span>
                             </a>
                         </li>
                         <li>
--- resources/js/Pages/landing.jsx
+++ resources/js/Pages/landing.jsx
@@ -1,35 +1,12 @@
 import React from "react";
-import { Link, Head } from '@inertiajs/inertia-react';
+import { Link, Head } from "@inertiajs/inertia-react";
+import Navbar from "@/Components/Navbar";
 
 export default function landing(props) {
     return (
         <>
             <Head title={props.judul} />
-            <div classname="carousel rounded-box">
-                <div classname="carousel-item">
-                    <img src="https://placeimg.com/400/300/arch" alt="Burger" />
-                </div>
-                <div classname="carousel-item">
-                    <img src="https://placeimg.com/400/300/arch" alt="Burger" />
-                </div>
-                <div classname="carousel-item">
-                    <img src="https://placeimg.com/400/300/arch" alt="Burger" />
-                </div>
-                <div classname="carousel-item">
-                    <img src="https://placeimg.com/400/300/arch" alt="Burger" />
-                </div>
-                <div classname="carousel-item">
-                    <img src="https://placeimg.com/400/300/arch" alt="Burger" />
-                </div>
-                <div classname="carousel-item">
-                    <img src="https://placeimg.com/400/300/arch" alt="Burger" />
-                </div>
-                <div classname="carousel-item">
-                    <img src="https://placeimg.com/400/300/arch" alt="Burger" />
-                </div>
-            </div>
-            # Snap to center
-
+            <Navbar />
         </>
-    )
-};
+    );
+}
