33135d48facc78e49ead614c1ef7509069ea04d9
bagheera30 <rizkialfian30103@gmail.com>
2022-07-23 22:21:08 +0700

    update

---
+++ resources/js/Components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <footer className="footer footer-center p-4 bg-slate-900 text-neutral-content">
+            <div>
+                <p>Copyright © 2022 - Indo kumat</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
--- resources/js/Components/Navbar.jsx
+++ resources/js/Components/Navbar.jsx
@@ -1,8 +1,23 @@
 const Navbar = () => {
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-amber-100">
             <div className="flex-1">
-                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                <a className="btn btn-ghost normal-case text-xl">
+                    <svg
+                        width="50"
+                        height="53"
+                        viewBox="0 0 114 153"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            fillRule="evenodd"
+                            clipRule="evenodd"
+                            d="M114 0H0V151.78L56.5917 95.1878L114 152.596V0Z"
+                            fill="#D0CAB2"
+                        />
+                    </svg>
+                </a>
             </div>
             <div className="flex-none gap-2">
                 <div className="form-control">
@@ -14,7 +29,7 @@ const Navbar = () => {
                 </div>
                 <div className="dropdown dropdown-end">
                     <label
-                        tabindex="0"
+                        tabIndex="0"
                         className="btn btn-ghost btn-circle avatar"
                     >
                         <div className="w-10 rounded-full">
@@ -22,7 +37,7 @@ const Navbar = () => {
                         </div>
                     </label>
                     <ul
-                        tabindex="0"
+                        tabIndex="0"
                         className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
                     >
                         <li>
