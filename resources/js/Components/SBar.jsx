708234fb74090c838dc21bb4851ff77d9f308491
bagheera30 <rizkialfian30103@gmail.com>
2022-10-03 15:03:21 +0700

    update

--- resources/js/Components/SBar.jsx
+++ resources/js/Components/SBar.jsx
@@ -16,33 +16,30 @@ const SBar = () => {
                     </button>
                 </div>
                 <div className="h-screen right-0 inset-y-0 w-96 fixed bg-teal-300 md:block sm:hidden">
-                    <div className="h-90 mx-40 justify-center py-20">
+                    <div className="h-80 mx-40 justify-center py-20">
                         <BsPersonCircle size={"10rem"} />
                     </div>
-                </div>
-                <div>
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Your Email</span>
-                        </label>
-                        <label className="input-group input-group-vertical">
-                            <span>Email</span>
-                            <input
-                                type="text"
-                                placeholder="info@site.com"
-                                className="input input-bordered"
-                            />
-                        </label>
-                    </div>
-                    <div className="form-control">
-                        <label className="input-group input-group-vertical">
-                            <span>Password</span>
-                            <input
-                                type="password"
-                                placeholder="Your Password"
-                                className="input input-bordered"
-                            />
-                        </label>
+                    <div className="h-screen right-0  py-0 w-96 fixed bg-teal-300 md:block sm:hidden">
+                        <div className="form-control">
+                            <label className="input-group input-group-vertical">
+                                <span>Email</span>
+                                <input
+                                    type="text"
+                                    placeholder="Email"
+                                    className="input input-bordered"
+                                />
+                            </label>
+                        </div>
+                        <div className="form-control">
+                            <label className="input-group input-group-vertical">
+                                <span>Password</span>
+                                <input
+                                    type="password"
+                                    placeholder="Your Password"
+                                    className="input input-bordered"
+                                />
+                            </label>
+                        </div>
                     </div>
                 </div>
             </div>
